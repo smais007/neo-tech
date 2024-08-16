93f4f5b0ccee17fa81e22abfeeaab070fafca5f0
Smais Shawon <smaisshawon@gmail.com>
2024-08-16 17:26:59 +0600

    added navbar

--- src/Navigation/Nav.jsx
+++ src/Navigation/Nav.jsx
@@ -1,6 +1,30 @@
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
+import { FiHeart } from "react-icons/fi";
 import "./Nav.css";
+
 function Nav() {
-  return <div>Nav</div>;
+  return (
+    <nav>
+      <div className="nav-container">
+        <input
+          className="search-input px-5 py-2"
+          type="text"
+          placeholder="Enter your search shoes."
+        />
+      </div>
+      <div className="profile-container flex">
+        <a href="#">
+          <FiHeart className="nav-icons" />
+        </a>
+        <a href="">
+          <AiOutlineShoppingCart className="nav-icons" />
+        </a>
+        <a href="">
+          <AiOutlineUserAdd className="nav-icons" />
+        </a>
+      </div>
+    </nav>
+  );
 }
 
 export default Nav;
